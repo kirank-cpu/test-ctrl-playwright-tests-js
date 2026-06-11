2d9d1f067db5f03696b72c85f1893faa4fbb0ec4
kirank-cpu <kirankhsn1@gmail.com>
2026-06-12 02:49:02 +0530

    cleanup: remove sample export tests/auth/sample_login.spec.js

--- tests/auth/sample_login.spec.js
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require("@playwright/test");
-const { SampleLoginPage } = require("../../pages/sample_login_page.js");
-test("sample login", async ({ page }) => {
-  const lp = new SampleLoginPage(page);
-  await page.goto("https://example.com");
-});
