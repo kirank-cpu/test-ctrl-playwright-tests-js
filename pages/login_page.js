19beba5654a8f6312c64c526341e0d760344d6c0
kirank-cpu <kirankhsn1@gmail.com>
2026-06-12 13:52:08 +0530

    test-capture: export pages/login_page.js

--- pages/login_page.js
+++ pages/login_page.js
@@ -3,12 +3,11 @@ import { expect } from '@playwright/test';
 export class LoginPage {
   constructor(page) {
     this.page = page;
-    this.clickOnLoginLogoLocator = page.locator(`div.login_logo`);
   }
 
-  async clickOnLoginLogo() {
-    // Click on login logo
-    await this.clickOnLoginLogoLocator.click();
+  async launchTheApp() {
+    // Launch the app
+    await this.page.goto(`https://www.github.com`);
   }
 
 }
