971416efe09cdff89b73a61fb21efa6c233fc76c
kirank-cpu <kirankhsn1@gmail.com>
2026-06-12 19:10:37 +0530

    test-capture: export pages/login_page.js

--- pages/login_page.js
+++ pages/login_page.js
@@ -3,11 +3,41 @@ import { expect } from '@playwright/test';
 export class LoginPage {
   constructor(page) {
     this.page = page;
+    this.clickUserNameLocator = page.locator(`#user-name`);
+    this.fillUserNameFieldLocator = page.locator(`#user-name`);
+    this.fillPasswordFieldLocator = page.locator(`#password`);
+    this.clickLoginButtonLocator = page.locator(`#login-button`);
+    this.clickProductsLocator = page.locator(`//span[normalize-space()="Products"]`);
   }
 
   async launchTheApp() {
     // Launch the app
-    await this.page.goto(`https://www.github.com`);
+    await this.page.goto(`https://www.saucedemo.com/`);
+  }
+
+  async clickUserName() {
+    // Click "User Name"
+    await this.clickUserNameLocator.click();
+  }
+
+  async fillUserNameField() {
+    // Fill "User Name" field
+    await this.fillUserNameFieldLocator.fill(`standard_user`);
+  }
+
+  async fillPasswordField() {
+    // Fill "Password" field
+    await this.fillPasswordFieldLocator.fill(`secret_sauce`);
+  }
+
+  async clickLoginButton() {
+    // Click "Login Button"
+    await this.clickLoginButtonLocator.click();
+  }
+
+  async clickProducts() {
+    // Click "Products"
+    await expect(this.clickProductsLocator).toBeVisible();
   }
 
 }
