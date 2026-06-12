4d22ac591719c2d39d3a30ddac2e1dc74914fe45
kirank-cpu <kirankhsn1@gmail.com>
2026-06-12 08:13:46 +0530

    Test-2

--- tests/new_tests/demo_test.spec.js
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-import { DemoTestPage } from '../../pages/demo_test_page';
-
-test('demo test', async ({ page }) => {
-  const demotestpage = new DemoTestPage(page);
-
-  await demotestpage.launchTheApp();
-});
